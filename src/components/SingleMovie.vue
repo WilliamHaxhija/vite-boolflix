7401aab914f399b761f2f3c2455a1c069e99fb7d
WilliamHaxhija <williamhaxhija@gmail.com>
2024-04-17 20:36:54 +0200

    feat: add flags

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -12,6 +12,10 @@ export default {
         return {
             store
         };
+    },
+
+    methods: {
+
     }
 }
 
@@ -23,11 +27,29 @@ export default {
         <div class="card">
             <div>{{ movieInfo.title }}</div>
             <div>{{ movieInfo.original_title }}</div>
-            <div>{{ movieInfo.original_language }}</div>
+            <div v-if="movieInfo.original_language === 'en'">
+                <img src="../assets/img/flag-uk.png" alt="en-flag">
+            </div>
+            <div v-else-if="movieInfo.original_language === 'fr'">
+                <img src="../assets/img/flag-france.avif" alt="fr-flag">
+            </div>
+            <div v-else-if="movieInfo.original_language === 'it'">
+                <img src="../assets/img/flag-italy.png" alt="it-flag">
+            </div>
+            <div v-else-if="movieInfo.original_language === 'es'">
+                <img src="../assets/img/flag-spain.png" alt="es-flag">
+            </div>
+            <div v-else>{{ movieInfo.original_language }}</div>
             <div>{{ movieInfo.vote_average }}</div>
         </div>
     </div>
 
 </template>
+    
+<style scoped lang="scss">
 
-<style scoped lang="scss"></style>
+    img {
+        max-width: 10%;
+    }
+
+</style>
